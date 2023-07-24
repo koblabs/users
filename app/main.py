ccb50f03b11125337d464101f464efb9afe26e17
Nana Kwame <9naquame@gmail.com>
2023-07-24 00:04:17 -0230

    main.py underway

--- app/main.py
+++ app/main.py
@@ -1,3 +1,16 @@
-from fastapi import FastAPI
+from fastapi import APIRouter, Depends, FastAPI
+
+from app.config import Settings, get_config
+from app.version import __version__
 
 app = FastAPI()
+
+
+@app.get("/info")
+async def info(config: Settings = Depends(get_config)) -> dict:
+    return {
+        "app_name": config.app_name,
+        "admin_email": config.admin_email,
+        "repo": config.repo,
+        "version": __version__,
+    }
